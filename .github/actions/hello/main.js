f87b9a9585d341fd8c645d792b8efa31ddf150f5
Chuk Lee <chukmunnlee@gmail.com>
2021-10-14 08:43:46 +0800

    actions tested with act

--- .github/actions/hello/main.js
+++ .github/actions/hello/main.js
@@ -3,7 +3,7 @@ const github = require('@actions/github')
 
 // random error 1/3 of the time
 const prob = Math.floor(Math.random() * 100) 
-if (prob <= 33) {
+if (prob <= 20) {
 	core.setFailed(`Error probability: ${prob}%`)
 	throw new Error(`Error probability: ${prob}%`)
 }
@@ -14,4 +14,4 @@ console.info(`Hello ${whoToGreet}. How are you today?`)
 const greetTime = (new Date()).toUTCString()
 core.setOutput('time', greetTime)
 
-console.info(JSON.stringify(github, any, '\t'))
+console.info(JSON.stringify(github, '', '\t'))
